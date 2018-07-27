1272f70c142b38f2dd882f9f79037bc00d64b121
Penn Mackintosh <penn.mackintosh@gmail.com>
2018-07-27 19:20:33 +0100

    .

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var unirest = require('unirest');
 var bodyParser = require('body-parser');
 var assert = require('assert');
 app.use(bodyParser.json()); // for parsing application/json
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+app.use(bodyParser.urlencoded({'extended': true}));
 
 function getFLID(device) {
   var lookup = {'lld': '266525', 'leland': '266525', 'hi6250': '266525'};
@@ -18,14 +14,14 @@ function apiRoute(req, res) {
     console.log(response.body);
     assert.equal(response.body.STATUS, 1);
     assert.equal(response.body.CODE, 200);
-    result = [];
+    var result = [];
     response.body.DATA.files.forEach(function(file) {
       result.push({'incremental': file.md5hash.slice(0,10), 'api_level': 0, 'url': file.url, 'timestamp': file.upload_date, 'filename': file.name, 'channel': new RegExp('-').split(file.name)[3]});
     });
-    respons = {'id': null, 'result': result, 'error': null};
+    var respons = {'id': null, 'result': result, 'error': null};
     res.send(respons);
   });
-}
+};}
 
 
 app.get('/api/v1/:device', apiRoute);
