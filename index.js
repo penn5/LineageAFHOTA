f2bdf77834dda68803ea7b8a428f7f1f829a776a
Penn Mackintosh <penn.mackintosh@gmail.com>
2018-07-27 15:58:33 +0100

    .

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function getFLID(device) {
   var lookup = {'lld': '266525', 'leland': '266525', 'hi6250': '266525'};
   return lookup[device];
 function apiRoute(req, res) {
-  unirest.get('https://androidfilehost.com/api/?action=folder&flid='+getFLID(req.params['device']).end(function (response) {
+  unirest.get('https://androidfilehost.com/api/?action=folder&flid='+getFLID(req.params['device'])).end(function (response) {
     console.log(response.body);
     assert.equal(response.body.STATUS, 1);
     assert.equal(response.body.CODE, 200);
